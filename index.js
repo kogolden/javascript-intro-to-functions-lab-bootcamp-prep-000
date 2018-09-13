6da48e4927b739f029d99018b033cf9c56af6630
oharagolden@gmail.com <oharagolden@gmail.com>
2018-09-13 22:42:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-    console.log("I can't hear you!")
+    return ("I can't hear you!")
   }
   else if (string.toUpperCase() === string) {
-    console.log("YES INDEED!")
+    return ("YES INDEED!")
   }
   else if (string === "I love you, Grandma.") {
-    console.log("I love you, too.")
+    return ("I love you, too.")
   }
 }
