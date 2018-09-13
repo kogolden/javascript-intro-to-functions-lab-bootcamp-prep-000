7ad1d6f63ae18a92a7f8dac3ce3ce6e9ede2138e
oharagolden@gmail.com <oharagolden@gmail.com>
2018-09-13 22:18:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) { 
+  return string.toUpperCase() 
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  const spy = 'HELLO'
+  console.log(spy)
+}
+function logWhisper(string) {
+  const spy = 'hello'
+  console.log(spy)
+}
+function sayHiToGrandma(string) {
+  var lowercase = 'hello!'
+  var uppercase = 'HELLO!'
+  var loveyou = 'I love you, Grandma.'
+  if (sayHiToGrandma(lowercase) )
+  console.log("I can't hear you!") 
+}
